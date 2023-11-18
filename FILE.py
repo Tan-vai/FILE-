cb37e9ae4c8e48b9c09d7b0e49e34572b0e7a828
Tan-vai <135997970+Tan-vai@users.noreply.github.com>
2023-11-18 23:27:14 +0600

    Create FILE.py

---
+++ FILE.py
@@ -0,0 +1,12 @@
+import os,requests,sys
+
+print('\033[38;5;46m[\033[1;31m•\033[38;5;46m] ASSALAMUALAIKUM 32 bit USER ')
+os.system('clear')
+print('\033[38;5;46m[\033[1;31m•\033[38;5;46m] Follow me Facebook')
+os.system('xdg-open https://facebook.com/abutanim.chowdury') 
+
+os.system('git pull')
+
+import FILE
+
+FILE.login()
